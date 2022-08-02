0a91472036c181865626f1f860b2f1f17938aa91
Maneesh P M <manu.pm55@gmail.com>
2022-08-02 11:30:47 +0530

    Extract program logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, process};
-use std::fs;
-use std::error::Error;
+
+use greponacid::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,32 +11,9 @@ fn main() {
 
     println!("searching for: {}\n searching in: {}", config.query, config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = greponacid::run(config) {
         println!("Application Err: {}", e);
         process::exit(1);
     }
 }
 
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        Ok(Config {
-            query: args[1].clone(),
-            filename: args[2].clone()
-        })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("Read contents:\n{}", contents);
-    Ok(())
-}
