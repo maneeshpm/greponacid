1856e9820a39b23c46764370153ff981a0ef38a9
Maneesh P M <manu.pm55@gmail.com>
2022-08-04 11:05:53 +0530

    Add running logic and file reader

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 use std::fs;
 use std::error::Error;
+use std::vec;
 
+// Stuct to hold input
 pub struct Config {
     pub query: String,
     pub filename: String
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
+    // Create a new config storing user input
+    pub fn new(args: &[String]) -> Result<Config, &'static str> { if args.len() < 3 {
             return Err("Not enough arguments");
         }
         Ok(Config {
@@ -18,9 +20,37 @@ impl Config {
     }
 }
 
+// Actually call the search
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("Read contents:\n{}", contents);
+    for line in search_in_content(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+// Perform search on some content
+fn search_in_content<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut matches: Vec<&str> = vec![];
+    for line in content.lines() {
+        if line.contains(query) {
+            matches.push(line);
+        }
+    }
+
+    matches
+}
+
+// tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_content() {
+        let query = "igg";
+        let contents = "first line\nneed bigger tests\nthird line";
+        assert_eq!(vec!["need bigger tests"], search_in_content(&query, &contents))
+    }
 }
