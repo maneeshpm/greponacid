9c123918c26b224003b0fe81aea0ea2efd016cc4
Maneesh P M <manu.pm55@gmail.com>
2022-08-04 11:49:58 +0530

    Redirect error messages to stderr

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use greponacid::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Err parsing arguments: {}", err);
+        eprintln!("Err parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -13,7 +13,7 @@ fn main() {
     println!("-------------------------------------------------------");
 
     if let Err(e) = greponacid::run(config) {
-        println!("Application Err: {}", e);
+        eprintln!("Application Err: {}", e);
         process::exit(1);
     }
 }
